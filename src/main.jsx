23b587f62f94b2157a7c886035ab10547a1c9d5d
xanimnazehmedova <xanimnazaxmedova@gmail.com>
2025-06-11 14:07:17 +0400

    Switch to HashRouter for Vercel deploy test

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';  // BrowserRouter yerinə HashRouter import et
 import App from './App'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
